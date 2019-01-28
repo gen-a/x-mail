5a305511edaa06e360f4664da537c31cf1d1c46a
gen-a <pugachevsky@gmail.com>
2019-01-28 15:20:52 +0200

    Optimized NewMail Component

--- src/components/NewMailModalWindow/NewMailModalWindow.js
+++ src/components/NewMailModalWindow/NewMailModalWindow.js
@@ -1,8 +1,26 @@
-import React, {Fragment} from 'react';
-import ModalWindow from "../ModalWindow/ModalWindow";
-import NewMail from "../NewMail/NewMail";
-import {connect} from 'react-redux';
-import {CLOSE_NEW_MAIL_WINDOW} from '../../actions/layout';
+import React, {Fragment} from 'react'
+import ModalWindow from '../ModalWindow/ModalWindow'
+import {connect} from 'react-redux'
+import {reset} from 'redux-form'
+import {CLOSE_NEW_MAIL_WINDOW} from '../../actions/layout'
+import NewMailForm from '../NewMailForm/NewMailForm'
+
+
+
+
+const NewMailModalWindow = (props) => {
+    return (
+        <Fragment>
+            <ModalWindow
+                title="Create new mail"
+                onClose={() => (props.closeNewMailWindow())}
+                isOpen = {props.isOpen}
+            >
+                <NewMailForm />
+            </ModalWindow>
+        </Fragment>
+    );
+};
 
 const mapStateToProps = (state) => {
     return {
@@ -14,25 +32,9 @@ const mapDispatchToProps = (dispatch) => {
     return {
         closeNewMailWindow: () => {
             dispatch({type: CLOSE_NEW_MAIL_WINDOW});
+            dispatch(reset('newMail'));
         }
     }
-};
-
-
-const NewMailModalWindow = (props) => {
-    return (
-        <Fragment>
-            <ModalWindow
-                title="Create new mail"
-                onClose={() => (props.closeNewMailWindow())}
-                isOpen = {props.isOpen}
-            >
-                <NewMail
-                    isActive = {props.isOpen}
-                />
-            </ModalWindow>
-        </Fragment>
-    );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewMailModalWindow);
